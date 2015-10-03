c3f102a33573ace7a5a9738f6699a4dc0d36c4bc
Alexandre Araujo (KODING) <alexandreh.araujo@gmail.com>
2015-10-03 23:55:54 +0000

    add login and register routes. also, a middleware for all other paths

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,11 +10,17 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
+$app->get('/login', ['uses'=>'Auth\AuthController@login']);
+$app->post('/login', ['uses'=>'Auth\AuthController@authenticate']);
+$app->get('/signup', ['uses'=>'Auth\AuthController@signup']);
+$app->post('/signup', ['uses'=>'Auth\AuthController@register']);
 
-$app->get('/', ['uses'=>'AppController@main']);
-
-$app->post('/workday', ['uses'=>'WorkdayController@store', 'as' => 'workday.store']);
-
-$app->put('/workday/{id}', ['uses'=>'WorkdayController@update', 'as' => 'workday.update']);
-
-$app->delete('/workday/{id}', ['uses'=>'WorkdayController@destroy', 'as' => 'workday.destroy']);
+$app->group(['middleware' => 'auth', 'namespace' => 'App\Http\Controllers'], function ($app) {
+    $app->get('/', ['uses'=>'AppController@main']);
+    
+    $app->post('/workday', ['uses'=>'WorkdayController@store', 'as' => 'workday.store']);
+    
+    $app->put('/workday/{id}', ['uses'=>'WorkdayController@update', 'as' => 'workday.update']);
+    
+    $app->delete('/workday/{id}', ['uses'=>'WorkdayController@destroy', 'as' => 'workday.destroy']);
+});
