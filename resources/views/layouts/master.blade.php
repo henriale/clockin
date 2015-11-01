1c67257ccdaf1a17c3e8d47fc9b618444a3c5a5f
Alexandre Araujo <alexandreh.araujo@gmail.com>
2015-11-01 04:30:30 -0200

    Fix uninitialized angular modules and split long lines

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" ng-app="@yield('ng-app')">
 <head>
   <!-- Required meta tags always come first -->
   <meta charset="utf-8">
--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.master')
 
+@section('ng-app', 'app')
+
 @section('title')
   Sign Up
 @stop
@@ -18,34 +20,56 @@
   <div class="col-sm-12" ng-controller="SignupController">
     <form name="formSignup" class="form-signup" method="post" action="{{ url('signup') }}" novalidate>
       <input type="hidden" name="_token" value="{!! csrf_token() !!}">
+
       <label for="email" class="sr-only">Email address</label>
-      <input type="email" name="email" class="form-control" placeholder="Email address" ng-model="email" ng-class="" required>
+      <input ng-model="email" required
+             type="email"
+             name="email"
+             class="form-control"
+             placeholder="Email address">
 
       <label for="password" class="sr-only">Password</label>
-      <input type="password" name="password" class="form-control" placeholder="Password" ng-model="password" required ng-minlength="6" ng-change="passCheck()">
+      <input ng-minlength="6" ng-change="passCheck()" ng-model="password" required
+             type="password"
+             name="password"
+             class="form-control"
+             placeholder="Password">
 
       <label for="repeatPassword" class="sr-only">Repeat password</label>
-      <input type="password" name="passwordConfirmation" class="form-control" placeholder="Repeat password" ng-model="passwordConfirmation" required ng-change="passCheck()">
+      <input ng-model="passwordConfirmation" ng-change="passCheck()" required
+             type="password"
+             name="passwordConfirmation"
+             class="form-control"
+             placeholder="Repeat password">
       <br />
 
-      <div ng-show="!is.empty(formSignup.$error) && formSignup.$dirty && formSignup.$invalid" class="alert alert-danger alert-dismissible fade in" role="alert">
+      <div ng-show="!is.empty(formSignup.$error) && formSignup.$dirty && formSignup.$invalid"
+           class="alert alert-danger alert-dismissible fade in"
+           role="alert">
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
           <span aria-hidden="true">&times;</span>
           <span class="sr-only">Close</span>
         </button>
         <ul>
-          <li ng-show="formSignup.email.$invalid"><strong>Email</strong> must be valid.</li>
-          <li ng-show="formSignup.password.$error.required"><strong>Password</strong> must be provided.</li>
-          <li ng-show="formSignup.password.$error.minlength"><strong>Password</strong> must have 6 characters, at least.</li>
-          <li ng-show="formSignup.passwordConfirmation.$error.passCheck"><strong>Password confirmation</strong> must be the same as password.</li>
+          <li ng-show="formSignup.email.$invalid">
+            <strong>Email</strong> must be valid.
+          </li>
+          <li ng-show="formSignup.password.$error.required">
+            <strong>Password</strong> must be provided.
+          </li>
+          <li ng-show="formSignup.password.$error.minlength">
+            <strong>Password</strong> must have 6 characters, at least.
+          </li>
+          <li ng-show="formSignup.passwordConfirmation.$error.passCheck">
+            <strong>Password confirmation</strong> must be the same as password.
+          </li>
         </ul>
       </div>
       <br />
 
-      <button type="submit"
-              class="btn btn-lg btn-secondary-outline btn-block"
-              ng-disabled="formSignup.$invalid"
+      <button type="submit" class="btn btn-lg btn-secondary-outline btn-block"
               ng-class="{disabled: formSignup.$invalid, 'btn-success-outline':formSignup.$valid}"
+              ng-disabled="formSignup.$invalid"
               ng-click="formSignup.$valid && register()">
           Sign up!
       </button>
